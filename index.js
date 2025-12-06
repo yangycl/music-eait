73bcafc5792e0f634755168001e5c0f482f3c2bb
yangycl <yangyangyclee@gmail.com>
2025-12-06 10:15:55 +0800

    初版線上編輯器

---
+++ index.js
@@ -0,0 +1,93 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var score = document.getElementById("score");
+const audioCtx = new AudioContext();
+function setHzAndTime(soundStr) {
+    let hz = 0;
+    let time = 1;
+    for (let i = 0; i < soundStr.length; i++) {
+        switch (soundStr[i]) {
+            case "1":
+                hz = 261.6; // 低音 C
+                break;
+            case "2":
+                hz = 293.7; // 低音 D
+                break;
+            case "3":
+                hz = 329.6; // 低音 E
+                break;
+            case "4":
+                hz = 349.2; // 低音 F
+                break;
+            case "5":
+                hz = 392.0; // 低音 G
+                break;
+            case "6":
+                hz = 440.0; // 低音 A
+                break;
+            case "7":
+                hz = 493.9; // 低音 B
+                break;
+        }
+    }
+    return {
+        hz: hz,
+        time: time
+    };
+}
+class sound {
+    constructor(soundStr) {
+        this.soundStr = soundStr;
+        let set = setHzAndTime(soundStr);
+        this.hz = set.hz;
+        this.time = set.time;
+    }
+    draw() {
+        if (score instanceof HTMLTextAreaElement) {
+            score.value += this.soundStr;
+        }
+    }
+    play(startTime) {
+        const oscillator = audioCtx.createOscillator();
+        oscillator.type = 'sine';
+        oscillator.frequency.value = this.hz;
+        oscillator.connect(audioCtx.destination);
+        oscillator.start(startTime);
+        oscillator.stop(startTime + this.time);
+    }
+}
+var soundArr = [];
+function updateSoundArr(soundText) {
+    let soundTextArr = soundText.split(/\s+|\n/);
+    for (let partSoundText of soundTextArr) {
+        let waitPushObj = new sound(partSoundText);
+        soundArr.push(waitPushObj);
+    }
+}
+var playBtn = document.getElementById("playBtn");
+if (playBtn) {
+    playBtn.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
+        if (score instanceof HTMLTextAreaElement) {
+            updateSoundArr(score.value);
+        }
+        yield audioCtx.resume(); // 等待 AudioContext 啟動
+        audioCtx.resume().then(() => {
+            let currentTime = audioCtx.currentTime;
+            for (let soundData of soundArr) {
+                soundData.play(currentTime);
+                currentTime += soundData.time;
+            }
+        });
+    }));
+}
+else {
+    throw new Error("找不到playBtn");
+}
---
+++ index.ts
@@ -0,0 +1,101 @@
+var score = document.getElementById("score");
+interface hzAndTime{
+    hz:number;
+    time:number
+}
+const audioCtx = new AudioContext();
+
+function setHzAndTime(soundStr: string): hzAndTime {
+    let hz: number = 0;
+    let time: number = 1;
+    
+    for (let i = 0; i < soundStr.length; i++) {
+        switch (soundStr[i]) {
+            case "1":
+                hz = 261.6; // 低音 C
+                break;
+            case "2":
+                hz = 293.7; // 低音 D
+                break;
+            case "3":
+                hz = 329.6; // 低音 E
+                break;
+            case "4":
+                hz = 349.2; // 低音 F
+                break;
+            case "5":
+                hz = 392.0; // 低音 G
+                break;
+            case "6":
+                hz = 440.0; // 低音 A
+                break;
+            case "7":
+                hz = 493.9; // 低音 B
+                break;
+
+        }
+    }
+    
+    return {
+        hz: hz,
+        time: time
+    };
+}
+class sound{
+    soundStr:string;
+    hz:number;
+    time:number;
+    constructor(soundStr:string){
+        this.soundStr = soundStr;
+        let set = setHzAndTime(soundStr);
+        this.hz = set.hz;
+        this.time = set.time;
+        
+    }
+    draw():void{
+        if(score instanceof HTMLTextAreaElement){
+            score.value += this.soundStr; 
+        }
+    }
+    play(startTime: number): void {
+        const oscillator = audioCtx.createOscillator();
+        oscillator.type = 'sine';
+        oscillator.frequency.value = this.hz;
+        oscillator.connect(audioCtx.destination);
+
+        oscillator.start(startTime);
+        oscillator.stop(startTime + this.time);
+    }
+    
+
+}
+
+
+var soundArr:sound[] = [];
+function updateSoundArr(soundText:string){
+    let soundTextArr:string[] = soundText.split(/\s+|\n/);
+    for (let partSoundText of soundTextArr){
+        let waitPushObj = new sound(partSoundText);
+        soundArr.push(waitPushObj); 
+    }
+}
+var playBtn = document.getElementById("playBtn");
+if(playBtn){
+    playBtn.addEventListener("click", async () => {
+        if(score instanceof HTMLTextAreaElement){
+            updateSoundArr(score.value)
+        }
+        await audioCtx.resume();  // 等待 AudioContext 啟動
+        audioCtx.resume().then(() => {
+            let currentTime = audioCtx.currentTime;
+            for (let soundData of soundArr) {
+                soundData.play(currentTime);
+                currentTime += soundData.time;
+            }
+        });
+    
+    });
+}else{
+    throw new Error("找不到playBtn");
+}
+
